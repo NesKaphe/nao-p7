2ce6644462372981056308f115994a99045477a4
Regis-Gianas <yrg@pps.univ-paris-diderot.fr>
2014-02-20 15:32:47 +0100

    * Reunion du 20/02.

--- nao_mouv_bras.py
+++ nao_mouv_bras.py
@@ -5,47 +5,44 @@ import time
 from naoqi import ALProxy
 
 def main():
-	IP = "192.168.1.3"
-	PORT = 9559
+        IP = "192.168.1.3"
+        PORT = 9559
 
-	try:
-		motion = ALProxy("ALMotion", IP , PORT)
-		posture = ALProxy("ALRobotPosture", IP , PORT)
-		speech = ALProxy("ALTextToSpeech", IP , PORT)
+        try:
+                motion = ALProxy("ALMotion", IP , PORT)
+                posture = ALProxy("ALRobotPosture", IP , PORT)
+                speech = ALProxy("ALTextToSpeech", IP , PORT)
 
-		motion.wakeUp()
+                motion.wakeUp()
 
-
-
-
-		posture.goToPosture("StandInit",0.5)
-		
-	
-		motion.closeHand('RHand')
-
-		#names 	= "HeadYaw"
-		names	= "LShoulderPitch"
-		angles 	= -30.0*almath.TO_RAD
-		frac	= 0.1 #10% de le vitesse max
-
-		angle_courrant = motion.getAngles("LShoulderPitch" , True)
-		print "l'angle vaut :", angle_courrant
+                posture.goToPosture("StandInit",0.5)
 
 
-		motion.setAngles(names,angles,frac)
-		time.sleep(2)
-		#motion.setAngles(names,angle_courrant,frac)
+                motion.closeHand('RHand')
+
+                #names  = "HeadYaw"
+                names   = "LShoulderPitch"
+                angles  = -30.0*almath.TO_RAD
+                frac    = 0.1 #10% de le vitesse max
+
+                angle_courrant = motion.getAngles("LShoulderPitch" , True)
+                print "l'angle vaut :", angle_courrant
 
 
-		motion.setStiffnesses("LArm",0.05)
-		posture.goToPosture("Sit",0.5)
+                motion.setAngles(names,angles,frac)
+                time.sleep(2)
+                #motion.setAngles(names,angle_courrant,frac)
 
-	except Exception,e:
-		print "probleme"
-		speech.say("Oulala je vihun de beuguer!")
 
-	motion.rest()
-	speech.say("Au revoir !")
+                motion.setStiffnesses("LArm",0.05)
+                posture.goToPosture("Sit",0.5)
+
+        except Exception,e:
+                print "probleme"
+                speech.say("Oulala je vihun de beuguer!")
+
+        motion.rest()
+        speech.say("Au revoir !")
 
 
 main()
