88013a162f936d917689463d95f87ab8170f5df9
dias <alain.dias02@gmail.com>
2014-03-13 15:15:55 +0100

    debut de detection de cercle

--- experimentation/openCV_exp/opencv_webcam.py
+++ experimentation/openCV_exp/opencv_webcam.py
@@ -12,38 +12,47 @@ cv2.namedWindow("thresh",cv.CV_WINDOW_AUTOSIZE)
 
 #variables pour le trackbar
 h1=0
-s1=0
-v1=0
-h2=0
-s2=0
-v2=0
+s1=117
+v1=124
+h2=31
+s2=189
+v2=255
+minDist=20
 thresh = None #image apres le traitement 
 def rien(x):
 	pass
 	
 
 #creation des trackbar
-cv.CreateTrackbar("H1","cnt",h1,255,rien)
-cv.CreateTrackbar("H2","cnt",h2,255,rien)
-cv.CreateTrackbar("S1","cnt",s1,255,rien)
-cv.CreateTrackbar("S2","cnt",s2,255,rien)
-cv.CreateTrackbar("V1","cnt",v1,255,rien)
-cv.CreateTrackbar("V2","cnt",v2,255,rien)
-
+cv.CreateTrackbar("H_MIN","cnt",h1,255,rien)
+cv.CreateTrackbar("H_MAX","cnt",h2,255,rien)
+cv.CreateTrackbar("S_MIN","cnt",s1,255,rien)
+cv.CreateTrackbar("S_MAX","cnt",s2,255,rien)
+cv.CreateTrackbar("V_MIN","cnt",v1,255,rien)
+cv.CreateTrackbar("V_MAX","cnt",v2,255,rien)
+cv.CreateTrackbar("minDistCircle","cnt",minDist,20,rien)
 
 while True:
 	ret,im = cap.read()
 	im2 = cv2.cvtColor(im,cv2.COLOR_BGR2HSV)
-	h1 = cv2.getTrackbarPos('H1','cnt')
-	h2 = cv2.getTrackbarPos('H2','cnt')
-	s1 = cv2.getTrackbarPos('S1','cnt')
-	s2 = cv2.getTrackbarPos('S2','cnt')
-	v1 = cv2.getTrackbarPos('V1','cnt')
-	v2 = cv2.getTrackbarPos('V2','cnt')
+	h1 = cv2.getTrackbarPos('H_MIN','cnt')
+	h2 = cv2.getTrackbarPos('H_MAX','cnt')
+	s1 = cv2.getTrackbarPos('S_MIN','cnt')
+	s2 = cv2.getTrackbarPos('S_MAX','cnt')
+	v1 = cv2.getTrackbarPos('V_MIN','cnt')
+	v2 = cv2.getTrackbarPos('V_MAX','cnt')
+	minDist = cv2.getTrackbarPos('minDistCircle','cnt')
 	mini = np.array([h1,s1,v1],np.uint8)
 	maxi = np.array([h2,s2,v2],np.uint8)
+	im = cv2.medianBlur(im,15)
     	cv2.imshow('video test',im)
 	thresh = cv2.inRange(im2,mini,maxi)
+	thresh = cv2.medianBlur(thresh,5)
+	circles = cv2.HoughCircles(thresh,cv.CV_HOUGH_GRADIENT,1,minDist,param1=50,param2=30,minRadius=0,maxRadius=0)
+	if circles != None:
+		circles = np.uint16(np.around(circles))
+		print "owi"
+	
 	cv2.imshow('thresh',thresh)
 	key = cv2.waitKey(10)
 
