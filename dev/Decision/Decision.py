a8594c06823457a0904413108401c8e1e5f3fb90
CHARASSON clement <charasso@informatique.univ-paris-diderot.fr>
2014-04-04 15:16:07 +0200

    ajout du gitignore rechercher de balle qui marche (pas très efficace pour le moment)

--- dev/Decision/Decision.py
+++ dev/Decision/Decision.py
@@ -8,7 +8,9 @@ from naoqi import ALProxy
 from Analyse.Analyse import *
 from Analyse.Vision import *
 from Mouvement.Mouvement import *
-
+import math
+import almath
+import time
 
 class Decision:
     def __init__(self):
@@ -23,15 +25,27 @@ class Decision:
     def rootine (self):
         #phase de calibrage
         a = Analyse(self.videoProxy)
-        a.filtre.calibrage()
+        #a.filtre.calibrage()
         
+        angle_vision = (60*math.pi)/180 #angle de vision en radian
+        px_vision = 640     
         
-        
+        px_to_ang = angle_vision/640
+
         mh = Head(self.motion,self.posture)
+        mh.reset()
         while True :
             DirBalle = a.BallPosition()
-            a.afficheImagesCourantes()
-            print "DirBalle",DirBalle
+            #a.afficheImagesCourantes()
+            print "angle reel",(self.motion.getAngles("HeadYaw",True)[0]*180.0)/math.pi#," , ",self.motion.getAngles("HeadPitch",True)
+            #print "DirBalle",DirBalle
+            
+            if DirBalle is not None :
+                #x= -((60*DirBalle[0])/640)
+                x = -(DirBalle[0]* px_to_ang)#+(math.pi /2)
+                print "angle calculé =",(x*180)/math.pi
+                mh.incrAnglesTo(x,0.0)
+                #time.sleep(1)
             #mh.incrAngle(DirBalle[0],DirBalle[1])
         
     
--- dev/Mouvement/Mouvement.py
+++ dev/Mouvement/Mouvement.py
@@ -11,6 +11,7 @@ import cv2.cv as cv
 import numpy as np
 import time
 import almath
+import math#contien pi
 
 
 IP = "192.168.1.3"
@@ -20,25 +21,54 @@ class Head:
 	def __init__(self,motion,posture):
 		self.motion = motion
 		self.posture = posture
-		self.angleYaw = 0.0 #récupérer les angles plustôt que ça
-		self.anglePitch = 0.0 #récupérer les angles plustôt que ça
 		#correspond au temps/vitesse pour atteindre l'angle voulut
 		self.incrSpeeds = 0.05
 		self.step = 0.02 #correspond au "pas" précis
-		
-	def turnTo(self,angleX,angleY,speedX=1.5,speedY=1.5) :#va donner les angles directement a la tête (angle en degré)
+	
+	def getYawAngle (self):#retourne l'angle Yaw en radian
+		return self.motion.getAngles("HeadYaw",True)[0]
+
+	def getPitchAngle (self):#retourne l'angle Pitch en radian
+		return self.motion.getAngles("HeadPitch",True)[0]
+
+
+	def turnTo(self,angleYaw,anglePitch,speedX=1.5,speedY=1.5) :#va donner les angles directement a la tête (angle en radian)
 		self.motion.setStiffnesses("Head", 1.0)
-		angleYaw= angleX*almath.TO_RAD
-		anglePitch = angleY*almath.TO_RAD
+		names  = ["HeadYaw", "HeadPitch"]
+		angles = [angleYaw, anglePitch]
+		times  = [speedX, speedY]
+		isAbsolute = True#?
+		self.motion.angleInterpolation(names, angles, times, isAbsolute)
+		#ici voir si il y a besion de relacher le stiffnesse
+		print "mouvement tete - fini"		
+
+
+	def incrAnglesTo(self,angleYaw,anglePitch,speedX=1.5,speedY=1.5) :#va donner les angles directement a la tête (angle en radian)
+		self.motion.setStiffnesses("Head", 1.0)
+
+		angleYaw += self.getYawAngle()
+		anglePitch += self.getPitchAngle()
 
 		names  = ["HeadYaw", "HeadPitch"]
 		angles = [angleYaw, anglePitch]
 		times  = [speedX, speedY]
-		isAbsolute = True#??
+		isAbsolute = True#?
 		self.motion.angleInterpolation(names, angles, times, isAbsolute)
 		#ici voir si il y a besion de relacher le stiffnesse
 		print "mouvement tete - fini"
+
+
 		
+	def incrAngles(self,dirX,dirY) :#incrémenter l'angle d'un "pas"(step)
+		self.motion.setStiffnesses("Head", 1.0) 
+		names  = ["HeadYaw", "HeadPitch"]
+		angleYaw =  self.getYawAngle() + self.angleYaw+(dirX*self.step)
+		anglePitch = self.getPitchAngle() + self.anglePitch+(dirY*self.step)
+		angles = [angleYaw,anglePitch]
+		times  = [self.incrSpeeds, self.incrSpeeds]
+		isAbsolute = True#?
+		self.motion.angleInterpolation(names, angles, times, isAbsolute)	
+
 		
 	def reset(self) :#met la tete à l'angle 0,0
 		self.motion.setStiffnesses("Head", 1.0) 
@@ -48,18 +78,6 @@ class Head:
 		isAbsolute = True#??
 		self.motion.angleInterpolation(names, angles, times, isAbsolute)
 		print "reset"
-		
-	def incrAngles(self,dirX,dirY) :#incrémenter l'angle d'un "pas"(step)
-		self.motion.setStiffnesses("Head", 1.0) 
-		names  = ["HeadYaw", "HeadPitch"]
-		self.angleYaw = self.angleYaw+(dirX*self.step)
-		self.anglePitch = self.anglePitch+(dirY*self.step)
-		angles = [self.angleYaw,self.anglePitch]
-		times  = [self.incrSpeeds, self.incrSpeeds]
-		isAbsolute = True#??
-		self.motion.angleInterpolation(names, angles, times, isAbsolute)	
-
-
 
 
 	def test (self):#petit teste pour faire des mouvements de tete
